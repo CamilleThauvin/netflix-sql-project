dce2d676e3317df14b04e1fcc8151360a6b6b948
Camille Thauvin <camillethauvin@mac.home>
2025-12-18 15:27:15 +0100

    feat: populate analytics queries with churn + revenue metrics

---
+++ tables/analytics/01_film_metrics.sql
@@ -0,0 +1,9 @@
+-- 01_film_metrics.sql
+-- Requêtes d'analyse sur les films / contenus (à compléter quand la table films sera créée)
+
+-- Exemple futur :
+-- SELECT category_name, COUNT(*) AS nb_titles
+-- FROM films f
+-- JOIN categorie c ON f.categorie_id = c.categorie_id
+-- GROUP BY category_name
+-- ORDER BY nb_titles DESC;
