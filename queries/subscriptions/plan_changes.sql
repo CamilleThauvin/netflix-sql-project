04c669ff1fee893bd66959674bb3d70027bc0988
Lucas Goumard <goumard.lucas@gmail.com>
2026-01-11 13:55:32 +0100

    feat: modification de l'analyse de changement des plans de souscription

--- queries/subscriptions/plan_changes.sql
+++ queries/subscriptions/plan_changes.sql
@@ -3,16 +3,23 @@ SELECT
     c.customer_id,
     c.first_name,
     c.last_name,
-    COUNT(cs.customer_subscription_id) - 1 AS plan_changes_count
+    c.email,
+    COUNT(cs.customer_subscription_id) AS total_subscriptions,
+    COUNT(cs.customer_subscription_id) - 1 AS plan_changes_count,
+    CASE
+        WHEN COUNT(cs.customer_subscription_id) = 1 THEN 'Aucun changement'
+        WHEN COUNT(cs.customer_subscription_id) = 2 THEN '1 changement'
+        ELSE CAST(COUNT(cs.customer_subscription_id) - 1 AS VARCHAR) || ' changements'
+    END AS status
 FROM customer_subscriptions cs
 JOIN customers c
     ON c.customer_id = cs.customer_id
 GROUP BY
     c.customer_id,
     c.first_name,
-    c.last_name
-HAVING
-    COUNT(cs.customer_subscription_id) > 1
+    c.last_name,
+    c.email
 ORDER BY
-    plan_changes_count DESC;
-
+    plan_changes_count DESC,
+    c.customer_id
+LIMIT 20;
